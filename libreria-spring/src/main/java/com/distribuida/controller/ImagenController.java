2c2642ed080bf4d58d7b00c4dab2e9144beb76cc
EP <epulluquitin@tecnologicoismac.edu.ec>
2025-09-05 08:49:58 -0500

    arreglo de imagenes

--- libreria-spring/src/main/java/com/distribuida/controller/ImagenController.java
+++ libreria-spring/src/main/java/com/distribuida/controller/ImagenController.java
@@ -19,30 +19,32 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api")
 public class ImagenController {
-    private static final String UPLOAD_DIR="uploads/portadas";
+
+    private static final String UPLOAD_DIR="uploads/portadas/";
 
     @PostMapping("/upload-portada")
     public ResponseEntity<Map<String, String>> uploadPortada(
-            @RequestParam("file")MultipartFile file,
-            @RequestParam(value="oldImage", required = false)String oldImage
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(value="oldImage", required = false) String oldImage
     ){
         try{
             Files.createDirectories(Paths.get(UPLOAD_DIR));
-            String filename= UUID.randomUUID() + "_"+file.getOriginalFilename();
+            String filename= UUID.randomUUID() + "_" + file.getOriginalFilename();
             Path path = Paths.get(UPLOAD_DIR + filename);
             Files.write(path, file.getBytes());
 
-            if (oldImage != null && !oldImage.isEmpty()){
+            if(oldImage != null && !oldImage.isEmpty()){
                 Path oldImagePath = Paths.get(oldImage);
-                Files.deleteIfExists(oldImagePath  );
+                Files.deleteIfExists(oldImagePath);
             }
+
             Map<String, String> response = new HashMap<>();
             response.put("ruta","portadas/" + filename);
             return ResponseEntity.ok(response);
-        }catch (IOException e){
-            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Map.of("error","Error al subir imagen"+ e.getMessage()));
-        }
 
+        }catch (IOException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error","Error al subir imagen" + e.getMessage()));
+        }
     }
 }
